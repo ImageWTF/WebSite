ca1b3a0d0055c93e7af6aa7e984c0d8afda24c9b
Nikola Hristov <nikola@nikolahristov.tech>
2023-02-22 05:05:49 +0200

    squash!

--- src/scripts/firebase.ts
+++ src/scripts/firebase.ts
@@ -2,13 +2,12 @@ import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
 	apiKey: "AIzaSyDBmnQxVvBOEqdr5498xb7Nb86WO5xA9f4",
-	appId: "1:199878156405:web:6adce1a48bdf961fda983c",
 	authDomain: "image-wtf-web.firebaseapp.com",
-	measurementId: "G-XFRD8Q5GFM",
-	messagingSenderId: "199878156405",
 	projectId: "image-wtf-web",
 	storageBucket: "image-wtf-web.appspot.com",
+	messagingSenderId: "199878156405",
+	appId: "1:199878156405:web:6adce1a48bdf961fda983c",
+	measurementId: "G-XFRD8Q5GFM",
 };
-
 
 initializeApp(firebaseConfig);
